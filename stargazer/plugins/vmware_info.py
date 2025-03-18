990da9c88a9e764cb36e0d9699ef4f781378f1aa
windyzhao <1129710372@qq.com>
2025-03-18 17:14:35 +0800

    feature-修改上报的数据格式为prometheus格式

--- stargazer/plugins/vmware_info.py
+++ stargazer/plugins/vmware_info.py
@@ -21,7 +21,6 @@ class VmwareManage(object):
         self.ssl = params.get("ssl", "false") == "true"
         self.si = self.connect_vc()
         self.content = self.si.RetrieveContent()
-        self.vc_name = self.content.about.name
 
     def test_connection(self):
         """
@@ -88,7 +87,6 @@ class VmwareManage(object):
 
                 result.append(
                     {
-                        "instance_id": self.vc_name,
                         "ip_addr": ip_addr,
                         # "inst_name": host.name,
                         "inst_name": f"{host.name}[{host._moId}]",
@@ -124,7 +122,6 @@ class VmwareManage(object):
                     continue
 
                 vm_dict = {
-                    "instance_id": self.vc_name,
                     "resource_id": vm._moId,
                     "inst_name": f"{vm.name}[{vm._moId}]",
                     "ip_addr": "",
@@ -200,7 +197,6 @@ class VmwareManage(object):
                 for datastore in datacenter.datastore:
                     datastore_list.append(
                         {
-                            "instance_id": self.vc_name,
                             "resource_id": datastore._moId,
                             "url": datastore.summary.url,
                             # "inst_name": datastore.summary.name,
@@ -217,13 +213,14 @@ class VmwareManage(object):
         return datacenters_list, datastore_list
 
     def service(self):
+        vc_name = self.content.about.name
         vc_version = self.content.about.version
         datacenters, datastore = self.get_datacenters_and_datastore()
         vm_list = self.get_vms()
         esxi = self.get_hosts()
 
         result = {
-            "vmware_vc": [{"vc_version": vc_version, "inst_name": self.vc_name, "instance_id": self.vc_name}],
+            "vmware_vc": [{"vc_version": vc_version, "inst_name": vc_name}],
             "vmware_ds": datastore,
             "vmware_vm": vm_list,
             "vmware_esxi": esxi,
